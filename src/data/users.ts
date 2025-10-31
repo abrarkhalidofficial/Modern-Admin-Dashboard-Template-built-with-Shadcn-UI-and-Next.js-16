0fe83d18238ced46ab60ec31dd89f81c8ad2899d
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2025-10-31 10:32:30 +0500

    base done

---
+++ src/data/users.ts
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    name: "Abrar Khalid",
+    username: "abrarkhalidofficial",
+    email: "hello@devscot.com",
+    avatar: "/avatars/abrar.png",
+    role: "administrator",
+  },
+  {
+    id: "2",
+    name: " M Abrar Khalid",
+    username: "abrarkhalidofficial",
+    email: "iabrarkhalidofficial@gmail.com",
+    avatar: "",
+    role: "admin",
+  },
+];
+
+export const rootUser = users[0];
